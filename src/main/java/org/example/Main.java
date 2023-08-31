ee3e97bd6e8ad1df013fa1980d5a9f7dbfe36b4d
wodls1616 <wodls1616@gmail.com>
2023-08-31 15:39:01 +0900

    프로젝션은 select 절에 조회할 대상을 지정하는것을 말한다. 프로젝션 대상은 엔티티, 임베디드 타입, 스칼라 타입이다.
    프로젝션으로 인해 반환타입을 지정하지 못할경우 new 명령어로 DTO를 조회해서 감싸서 반환타입을 설정할 수 있다.
    다른 방법인 Object[] 타입과 Query타입이 있지만 new 명령어로 DTO를 조회해서 지정하는게 가장 편하다

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,9 @@
 package org.example;
 
+import org.example.jpql.Address;
 import org.example.jpql.Member;
+import org.example.jpql.MemberDTO;
+import org.example.jpql.Team;
 
 import javax.persistence.*;
 import java.util.List;
@@ -21,33 +24,20 @@ public class Main {
             member.setAge(10);
             em.persist(member);
 
-            // Member 엔티티의 데이터 전체를 가져오기 때문에 Member로 타입 변환
-            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
-            // 결과가 1개 이상일때 getResultList()로 리스트로 반환  값이 없으면 빈 리스트 반환
-            List<Member> resultList = query1.getResultList();
-
-            for (Member member1 : resultList) {
-                System.out.println("member = " + member1);
-            }
-
-            // 결과가 1개일 때 getSingleResult()로 단일 객체 반환
-            // 결과가 없으면 없다는 오류 출력
-            // 2개이상이면 noUnique 오류 출력
-            Member result = query1.getSingleResult();
-            System.out.println("result = " + result);
-
-            // String 1개만 넘어오기 떄문에 String 으로 타입 변환
-            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
-            // String과 int형 2개를 가져오기 떄문에 타입변환을 설정할 수 없다.
-            Query query3 = em.createQuery("select m.username, m.age from Member m");
-
-            // 파라미터 바인딩 - 이름기준
-            Member result1 = em.createQuery("select m from Member m where m.username =: username", Member.class)
-                    .setParameter("username", "member1")
-                    .getSingleResult();
-            System.out.println("result1 = " + result1.getUsername());
+            em.flush();
+            em.clear();
 
 
+            List<MemberDTO> result = em.createQuery("select new org.example.jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+                    .getResultList();
+
+            MemberDTO memberDTO = result.get(0);
+            System.out.println("MemberDTO = " + memberDTO.getUsername());
+            System.out.println("MemberDTO = " + memberDTO.getAge());
+
+          /*  Member findMember = result.get(0);
+            findMember.setAge(20);
+*/
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
