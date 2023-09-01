2859f0aaa7e8a3a5615e63c86687b8e435e908a8
wodls1616 <wodls1616@gmail.com>
2023-09-02 02:47:59 +0900

    JPQL을 활용하여 join 처리 left join, inner join등을 해줄수 있다.
    select m from Member m left join m.team t으로 조인 가능(left outer join)
    select m from Member m join m.team t으로 조인 가능(inner join)
    
    on절로 연관관계가 없는 엔티티 끼리 조인도 가능하다.
    SELECT m, t FROM
    Member m LEFT JOIN Team t on m.username = t.name

--- src/main/java/org/example/jpql/Member.java
+++ src/main/java/org/example/jpql/Member.java
@@ -20,6 +20,11 @@ public class Member {
     @JoinColumn(name = "TEAM_ID")
     private Team team;
 
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 
