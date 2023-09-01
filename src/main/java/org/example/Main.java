5355e09af4d115ccedaab247efb4f59cc33669bc
wodls1616 <wodls1616@gmail.com>
2023-09-01 23:42:45 +0900

    JPQL을 활용하여 페이징 기능 처리 setFirstResult()와 setMaxResults()를 사용하여 페이징 기능 처리 setFirstResult()은 시작 위치를 지정 setMaxResults()은 조회할 데이터의 수를 지정한다.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,11 @@
 package org.example;
 
-import org.example.jpql.Address;
 import org.example.jpql.Member;
-import org.example.jpql.MemberDTO;
-import org.example.jpql.Team;
 
-import javax.persistence.*;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 import java.util.List;
 
 public class Main {
@@ -18,22 +18,25 @@ public class Main {
         tx.begin();
 
         try {
-
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-            em.persist(member);
+            for (int i = 0; i < 100; i++ ) {
+                Member member = new Member();
+                member.setUsername("member" + i);
+                member.setAge(i);
+                em.persist(member);
+            }
 
             em.flush();
             em.clear();
 
-
-            List<MemberDTO> result = em.createQuery("select new org.example.jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(10)
                     .getResultList();
 
-            MemberDTO memberDTO = result.get(0);
-            System.out.println("MemberDTO = " + memberDTO.getUsername());
-            System.out.println("MemberDTO = " + memberDTO.getAge());
+            System.out.println("result size = " + result.size());
+            for (Member member1 : result) {
+                System.out.println("member1 = " + member1);
+            }
 
           /*  Member findMember = result.get(0);
             findMember.setAge(20);
--- src/main/java/org/example/jpql/Member.java
+++ src/main/java/org/example/jpql/Member.java
@@ -54,4 +54,13 @@ public class Member {
     public void setTeam(Team team) {
         this.team = team;
     }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
